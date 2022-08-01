76e56c86cc2c5693cae70cf11d68557e26efdf2b
Rafael Cazarotto <rafaelcaza1997@gmail.com>
2022-08-01 14:26:39 -0300

    adiçao de novas funcionalidades

--- projetos/admin.py
+++ projetos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from projetos.models import Projeto
+
+class ProjetoAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Projeto, ProjetoAdmin)
--- projetos/urls.py
+++ projetos/urls.py
@@ -3,7 +3,9 @@ from django.urls import include, path
 from projetos import views
 
 
+app_name = 'projetos'
+
 urlpatterns = [
     path('', views.view_all_projects),
-    path('<int:pk>', views.project_detail),
+    path('<int:pk>', views.project_detail, name = 'detalhes_projeto'),
 ]
